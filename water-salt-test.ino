2f719a6d0ff8b187268fdb25cc042211bd801169
isaac-newt <isaac.newt.list@icloud.com>
2017-11-23 13:59:25 -0600

    enable LCD printing

--- water-salt-test.ino
+++ water-salt-test.ino
@@ -10,16 +10,31 @@
  *       Water Purifier project
  */
 
-// Set up pins
-int saltPin = 1;
-int cleanPin = 2;
-int warnLight = 3;
-int safeLight = 4;
+ /*
+  * LCD Setup:
+  *
+  * LCD RS pin to digital pin 12
+  * LCD Enable pin to digital pin 11
+  * LCD D4 pin to digital pin 5
+  * LCD D5 pin to digital pin 4
+  * LCD D6 pin to digital pin 3
+  * LCD D7 pin to digital pin 2
+  * LCD R/W pin to ground
+  * LCD VSS pin to ground
+  * LCD VCC pin to 5V
+  */
 
-/*
- * Would need to set up stuff for displaying on an
- * LCD display here as well.  Haven't done that yet.
- */
+#include <LiquidCrystal.h>
+
+// Set up LCD pins
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// Set up remaining pins
+int saltPin = 6;
+int cleanPin = 7;
+int warnLight = 8;
+int safeLight = 9;
 
 // Define variables
 int saltValue;
@@ -31,12 +46,18 @@ void setup() {
   pinMode(cleanPin, INPUT);
   pinMode(warnLight, OUTPUT);
   pinMode(safeLight, OUTPUT);
+  // Set up LCD
+  lcd.begin(16, 2);
+  // Test message
+  lcd.print("Hello World");
 }
 
 // loop the needed functions
 void loop() {
   checkClean();
   checkSalt();
+  // May want a delay here, e.g
+  // "delay(250);" for .25sec's
 }
 
 // Check salinity of clean water
@@ -44,9 +65,9 @@ void checkClean() {
   // check resistance of water
   cleanValue = digitalRead(cleanPin);
 
-  // print value of clean water to serial monitor
-  // replace this with an output to a display
+  // print value of clean water to serial monitor and LCD
   Serial.print(cleanValue);
+  lcd.print(cleanValue);
 
   // what to do based on cleanValue
   if (cleanValue >= 2) {
@@ -64,9 +85,9 @@ void checkSalt() {
   // check resistance of saltwater
   saltValue = digitalRead(saltPin);
 
-  // print value of clean water to serial monitor
-  // replace this with an output to a display
+  // print value of clean water to serial monitor and LCD
   Serial.print(saltValue);
+  lcd.print(saltValue);
 
   // what to do based on saltValue
   // **Do we need any warnings for the saltwater?**
